c8cad85e0654071a3a1691a74cde7bb91269db49
antoniogomez2015 <monoproduccion@gmail.com>
2018-09-30 12:11:36 -0430

    Se ha inicializado el repositorio de Wordpres, 30 de Septiembre del 2018

---
+++ projectProff/source/js/desarrollo.js
@@ -0,0 +1,39 @@
+
+
+$(document).ready(function(){
+
+	/*slideshow sistem*/
+	$('.bxslider').bxSlider({
+
+		/*dirección de la transición 'horizontal', 'vertical', 'fade'*/
+		/*mode: 'fade',*/
+
+		/*reproducción automática*/
+		auto: true,
+
+		/*controles ó manejadores laterales*/
+		/*controls: false,*/
+
+		/*icons, ingeriores de navegación*/
+		pager: false,
+
+		/*duración de visualización de cada slide*/
+		 pausa: 15000,
+
+		/*duración de la transición entre slides*/
+		speed: 2000,
+
+		/*comienzo aleatorio para el slideshow*/
+		randomStart: true,
+
+		/*interpolación de animación, opciones 'swing', 'linear'*/
+		easing: 'swing',
+
+		/*opción de visualización de tipo filmstrip, ó news*/
+		/*ticker: true,*/		  		
+
+	  });
+
+	/*-----------------------------------------------------------------------------------------*/
+
+});
